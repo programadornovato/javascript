7b2ffbb89cd58ef40d06be99a601309db1f2ad31
Programador Novato <info@programadornovato.com>
2019-07-06 14:47:24 -0500

    02.- Javascript🔥 de 0 a 100 [Variables]
    https://youtu.be/N_nT9gsnf0g

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-alert('Hola mundo');
+//alert('Hola mundo');
+var numero1=5;
+var numero2=15;
+var cadena='hola mundo';
+var res=numero1+numero2;
+//alert(typeof(res));
+//var numeroUno=100;
+//var numeroUno='Hola mundo';
+//alert(numeroUno);
+
+let numeroUno=100;
+numeroUno='Hola mundo';
+//alert(numeroUno);
+
+const numeroTres=333;
+//numeroTres=3;
+//alert(numeroTres);
+
+let numeroCuatro=44;
+//numeroCuatro=numeroCuatro+1;
+numeroCuatro--;
+numeroCuatro--;
+numeroCuatro--;
+numeroCuatro--;
+//alert(numeroCuatro);
+console.log(numeroCuatro);
+
