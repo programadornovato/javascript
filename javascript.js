c6c79a604a2d91cb2e0c39241daecec4a1aafaa6
Programador Novato <info@programadornovato.com>
2019-07-07 14:58:54 -0500

    05.- Javascript🔥 de 0 a 100 [ciclos for y while]
    https://youtu.be/Zj4BMBWx0wo

--- javascript.js
+++ javascript.js
@@ -1,24 +1,26 @@
-/*
-var array=[1,2,3,"hola",true,false];
+
+var array=[];
 //         0,1,2,3
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
-console.log(array[3]);
+/*
+for (var i=0;i<array.length;i++){
+    console.log(array[i]);
+}
 */
-let colores=new Array("rojo","azul","verde");
-//console.log(colores[0]);
-console.log(colores);
-//console.log(colores.toString());
-//console.log(colores.length);
-//colores.length=2;
-//colores[colores.length]="amarillo";
-//colores.push("amarillo");
-//colores.pop();
-//console.log(colores.shift());
-//colores.unshift("blanco");
-//colores.reverse();
-//colores.sort();
-//var colores2=colores.slice(2);
-colores.splice(1,2,"violeta","morado");
-console.log(colores);
+/*
+var i=0;
+while(i<array.length){
+    console.log(array[i]);
+    i++;
+}
+*/
+
+var i=0;
+do{
+    if(array[i]==null){
+        console.log('No ha elementos');
+    }
+    else{
+        console.log(array[i]);
+    }
+    i++;
+}while(i<array.length);
