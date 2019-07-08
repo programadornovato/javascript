ee68f66bab8b769990fc1ea17ed9fb7c4290fb70
Programador Novato <info@programadornovato.com>
2019-07-08 13:53:08 -0500

    07.- Javascript🔥 de 0 a 100 [funciones]
    https://youtu.be/GI8hfDP4gYw

--- javascript.js
+++ javascript.js
@@ -1,29 +1,45 @@
-var accion = "raiz";
-/*
-if(accion=="suma"){
-    console.log("se suma");
-}
-if(accion=="resta"){
-    console.log("se resta");
-}
-*/
+var accion = "div";
+var numero1 = 10;
+var numero2 = 0;
 switch (accion) {
     case "suma":
-        console.log("se suma");
+        console.log("La suma es " + suma(numero1, numero2));
         break;
     case "resta":
-        console.log("se resta");
+        console.log("La resta es " + resta(numero1, numero2));
         break;
 
     case "mul":
-        console.log("se multiplica");
+        console.log("La mul es " + mul(numero1, numero2));
         break;
 
     case "div":
-        console.log("se divide");
+            console.log("La div es " + div(numero1, numero2));
         break;
     default:
         console.log("no se encontro nada");
+}
+function suma(num1, num2) {
+    return num1 + num2;
+}
+function resta(num1, num2) {
+    if (num2 >= num1) {
+        return "El numero 2 no puede ser mayor al numero 1";
+    }
+    else {
+        return num1 - num2;
+    }
 
-
+}
+function mul(num1, num2) {
+    return num1 * num2;
+}
+function div(num1, num2) {
+    if(num2==0){
+        return "Numero 2 no puede ser 0";
+    }
+    else{
+        return num1 / num2;
+    }
+    
 }
