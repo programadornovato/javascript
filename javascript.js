04d8246da2fe07d907d11a7bf3340d106e3bc951
Programador Novato <info@programadornovato.com>
2019-07-09 19:14:20 -0500

    11.- Javascript🔥 de 0 a 100 [Objetos]
    https://youtu.be/tJ3E31UQIi8

--- javascript.js
+++ javascript.js
@@ -1,18 +1,44 @@
-let nombre=document.getElementById('nombre');
 /*
-function retornaNombre(nombre){
-    return 'Hola '+nombre;
+let arragle=[1,2,3,'hola'];
+console.log(arragle);
+
+let objeto={
+    nombre:'juan',
+    puesto:'programador',
+    edad:20,
+    proyectos:{
+        nombre:'tienda',
+        version:1.5,
+        des:'Una tienda online'
+    }
 }
+console.log(objeto.proyectos.nombre);
 */
-let retornaNombre = nombre => 'Hola '+nombre;
-nombre.innerHTML=retornaNombre('novato');
+let objetos=[
+    {
+        nombre:'juan',
+        puesto:'programador',
+        edad:20    
+    },
+    {
+        nombre:'maria',
+        puesto:'programador',
+        edad:25    
+    },
+    {
+        nombre:'juana',
+        puesto:'programador',
+        edad:22    
+    }
+];
+/*
+console.log(objetos[0].nombre);
+console.log(objetos[1].nombre);
+console.log(objetos[2].nombre);
+*/
+for(let i=0;i<objetos.length;i++){
+    console.log(objetos[i].nombre+' '+objetos[i].puesto);
+}
 
 
-let nombres=document.getElementById('nombres');
-/*
-function retornaNombres(nombre,apellido){
-    return 'Hola '+nombre+apellido;
-}
-*/
-let retornaNombres = (nombre,apellido) => nombre+' '+apellido;
-nombres.innerHTML+=' '+retornaNombres('Programador','novato');
+
