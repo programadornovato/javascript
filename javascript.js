6fc1fe7e942ac39aa595fe196d525dd5ccf686be
Programador Novato <info@programadornovato.com>
2019-07-10 17:20:35 -0500

    13.- Javascript🔥 de 0 a 100 [fetch]
    https://youtu.be/0c1Fu6nVUDs

--- javascript.js
+++ javascript.js
@@ -1,17 +1,8 @@
-let key="AIzaSyD-tT6sIqfOzIDjcTaC2elgXOwmCzkss1A";
-let idCanal="UCekiUWTObDKZnaeS-reYQbg";
-let resPorPagina=25;
-let url="https://www.googleapis.com/youtube/v3/search?key=" + key + "&channelId=" + idCanal + "&part=snippet,id&order=date&maxResults=" + resPorPagina;
-
-let xmlhttp=new XMLHttpRequest();
-xmlhttp.open('GET',url,true);
-xmlhttp.send();
-xmlhttp.onreadystatechange=function(){
-    if(this.readyState==4 && this.status==200){
-        let data=JSON.parse(this.responseText);
-        console.log(data);
-        for(var i=0;i<data.items.length;i++){
-            console.log(data.items[i].snippet.title);
-        }
-    }
+document.getElementById('boton').onclick=function(){
+    fetch('archivo.txt')
+    .then(data=>data.text())
+    .then(data=>{
+        //console.log(data);
+        document.getElementById('con').innerHTML=data;
+    })
 }
