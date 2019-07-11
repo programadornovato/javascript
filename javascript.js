40e016e1e983a48ce1c029ed8539718a995da53d
Programador Novato <info@programadornovato.com>
2019-07-11 01:36:09 -0500

    15.- Javascript🔥 de 0 a 100 [api fetch y json]
    https://youtu.be/cGmQjGIY05s

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 document.getElementById('boton').onclick=function(){
-    fetch('https://randomuser.me/api')
+    fetch('data.json')
     .then(data=>data.json())
     .then(data=>{
-        //console.log(data);
-        document.getElementById('imagen').src=data.results[0].picture.large;
-        document.getElementById('nombre').innerHTML=data.results[0].name.first;
+        console.log(data);
+        for(var i=0;i<data.length;i++){
+            document.getElementById('registros').innerHTML=document.getElementById('registros').innerHTML+
+            "<tr>"+
+                "<td scope='row'>"+data[i].id+"</td>"+
+                "<td>"+data[i].nombre+"</td>"+
+                "<td>"+data[i].puesto+"</td>"+
+                "<td>"+data[i].estado+"</td>"+
+            "</tr>";
+        }
     })
 }
