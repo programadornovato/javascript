83abc9962cb3c47ad0fa8135646106f0d602d6db
Programador Novato <info@programadornovato.com>
2019-07-13 02:49:37 -0500

    20.- Javascript🔥 de 0 a 100 [Llenar tabla con ajax]
    https://youtu.be/xlbfYikZxno

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
 let boton=document.getElementById('boton');
+let con=document.getElementById('con');
 boton.onclick=function(){
-    let xhttp=new XMLHttpRequest();
-    xhttp.open('GET',"archivo.txt",true);
-    xhttp.send();
-    xhttp.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200 ){
-            //console.log(this.responseText);
-            document.getElementById('con').innerHTML=this.responseText;
+    let xhtml=new XMLHttpRequest();
+    xhtml.open('GET','data.json',true);
+    xhtml.send();
+    xhtml.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            console.log(this.responseText);
+            let data=JSON.parse(this.responseText);
+            //console.log(data);
+            con.innerHTML='';
+            for(var i=0;i<data.length;i++){
+                //console.log(data[i].nombre);
+                con.innerHTML=con.innerHTML+
+                '<tr>'+
+                '<th scope="row">'+data[i].id+'</th>'+
+                '<td>'+data[i].nombre+'</td>'+
+                '<td>'+data[i].puesto+'</td>'+
+                '<td>'+data[i].estado+'</td>'+
+                '</tr>';
+
+            }
         }
     }
 }
