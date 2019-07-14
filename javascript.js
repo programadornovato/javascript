533f6baab97bcf4200d54f8ba923d819f65e7e33
Programador Novato <info@programadornovato.com>
2019-07-14 01:34:20 -0500

    22.- Javascript🔥 de 0 a 100 [Delegation]
    https://youtu.be/slq1MVqXjyo

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
-// IIFE Immediately Invoked Function Expression (Funciones que son invocadas inmediatamente)
-/*
-function trabaja(accion){
-    alert("Estoy trabajando en "+accion);
-}
-trabaja("compras");
-*/
-/*
-(function trabaja(accion){
-    alert("Estoy trabajando en ");
-})("programacion");
-*/
+let boton=document.getElementById('boton');
+let con=document.getElementById('con');
+let cuerpo=document.getElementById('cuerpo');
 
-((accion)=>{
-    alert("Estoy trabajando en "+accion);
-})("el bar mi oficina");
+boton.onclick=function(){
+    con.innerHTML=
+    '<button type="button" class="btn btn-primary">btn-primary</button>'+
+    '<button type="button" class="btn btn-secondary">btn-secondary</button>'+
+    '<button type="button" class="btn btn-danger">btn-danger</button>'+
+    '<button type="button" class="btn btn-success">btn-success</button>'+
+    '<button type="button" class="btn btn-warning">btn-warning</button>';
+}
+con.onclick=function(e){
+    //e.preventDefault();
+    //console.log(e.target.classList[1]); 
+    let clase=e.target.classList[1];
+    cuerpo.className=clase;
+}
 
