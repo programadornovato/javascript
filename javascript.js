d8306124d8684cf62d02032e5936b377dd7b85b4
Programador Novato <info@programadornovato.com>
2019-07-15 22:13:35 -0500

    25.- Javascript🔥 de 0 a 100 [CRUD con localstorage part 2]
    https://youtu.be/beXCiPlrbCo

--- javascript.js
+++ javascript.js
@@ -1,1 +1,38 @@
-alert("aqui estoy papa");
+let formulario=document.getElementById('formulario');
+let tarea=document.getElementById('tarea');
+let listaTareas=document.getElementById('listaTareas');
+
+let arrayTareas=[];
+
+let item={
+    tarea:"",
+    estado:false
+}
+
+function agregarTarea(texto){
+    item={
+        tarea:texto,
+        estado:false
+    }
+    arrayTareas.push(item);
+}
+/*
+agregarTarea("comer");
+agregarTarea("beber");
+console.log(arrayTareas);
+*/
+formulario.onsubmit=function(e){
+    e.preventDefault();
+    let textoTarea= tarea.value;
+    agregarTarea(textoTarea)
+    console.log(arrayTareas);
+    tarea.value="";
+    GuaradarBD();
+}
+function GuaradarBD(){
+    localStorage.setItem('tareas',JSON.stringify(arrayTareas));
+    MostrarBD();
+}
+function MostrarBD(){
+    console.log(localStorage.getItem('tareas'));
+}
